c4fb8c48c870d85e00d0d9585d13811d678be96c
cchambe12 <cchamberlain@g.harvard.edu>
2020-03-19 15:11:02 -0400

    updated code

--- leadingcode/ch12_ospexample.R
+++ leadingcode/ch12_ospexample.R
@@ -8,6 +8,8 @@ options(stringsAsFactors = FALSE)
 library(ggplot2)
 library(rethinking)
 
+set.seed(1221)
+
 ### Let's use OSPREE data
 setwd("~/Documents/git/bayes2020")
 osp <- read.csv("leadingcode/data/bbstan_mainmodel_utah_allsppwcrops_allfp_allchill.csv")
@@ -27,9 +29,8 @@ m_nopool <- map2stan(
     ),
     data=osp,
     chains=2, iter=2000, warmup=1500, 
-    control=list(max_treedepth = 15,adapt_delta = 0.99),
+    control=list(max_treedepth = 15,adapt_delta = 0.99)
   )
-)
 
 precis(m_nopool)
 
@@ -39,8 +40,8 @@ osp$species <- as.integer(as.factor(osp$complex.wname))
 m_partpool <- map2stan(
   alist(
     resp ~ dnorm(mu, sigma),
-    mu <- a + a[species] + bf*force,
-    a[species] ~ dnorm(0, sigma_species),
+    mu <- a + a_species[species] + bf*force,
+    a_species[species] ~ dnorm(0, sigma_species),
     a ~ dnorm(0, 50),
     bf ~ dnorm(0,50),
     sigma ~ dnorm(0,10),
@@ -48,9 +49,9 @@ m_partpool <- map2stan(
   ),
   data=osp,
   chains=2, iter=2000, warmup=1500, 
-  control=list(max_treedepth = 15,adapt_delta = 0.99),
+  control=list(max_treedepth = 15,adapt_delta = 0.99)
 )
-)
+
 
 precis(m_partpool, depth=2)
 precis(m_nopool)
@@ -66,20 +67,20 @@ osp$study <- as.numeric(as.factor(osp$datasetID))
 m_partpool_wstudy <- map2stan(
   alist(
     resp ~ dnorm(mu, sigma),
-    mu <- a + a[species] + a[study] + bf*force,
-    a[species] ~ dnorm(0, sigma_species),
-    a[study] ~ dnorm(0, sigma_study),
+    mu <- a + a_study[study] + a_species[species] + bf*force,
+    a_study[study] ~ dnorm(0, sigma_study),
+    a_species[species] ~ dnorm(0, sigma_species),
     a ~ dnorm(0, 50),
     bf ~ dnorm(0, 50),
     sigma ~ dnorm(0,10),
-    sigma_species ~ dnorm(0,10),
-    sigma_study ~ dnorm(0,10)
+    sigma_study ~ dnorm(0,10),
+    sigma_species ~ dnorm(0,10)
   ),
   data=osp,
   chains=2, iter=2000, warmup=1500, 
-  control=list(max_treedepth = 15,adapt_delta = 0.99),
+  control=list(max_treedepth = 15,adapt_delta = 0.99)
 )
-)
+
 
 
 dev.new()
@@ -94,44 +95,55 @@ plot(precis(m_partpool, depth=2))
 plot(precis(m_partpool_wstudy, depth=2))
 
 
-
+if(FALSE){
 ###### For full model, see here:
-if(FALSE){
-  m_nopool <- map2stan(
-    alist(
-      resp ~ dnorm(mu, sigma),
-      mu <- a + bf*force + bp*photo + bc*chill,
-      a ~ dnorm(0, 50),
-      bf ~ dnorm(0,50),
-      bp ~ dnorm(0,50),
-      bc ~ dnorm(0,50),
-      sigma ~ dnorm(0,10)
-    ),
-    data=osp,
-    chains=4, iter=2000, warmup=1500, 
-    control=list(max_treedepth = 15,adapt_delta = 0.99),
-  )
-  )
+m_nopool_full <- map2stan(
+  alist(
+    resp ~ dnorm(mu, sigma),
+    mu <- a + bf*force + bp*photo + bc*chill,
+    a ~ dnorm(0, 50),
+    bf ~ dnorm(0,50),
+    bp ~ dnorm(0,50),
+    bc ~ dnorm(0,50),
+    sigma ~ dnorm(0,10)
+  ),
+  data=osp,
+  chains=4, iter=4000, warmup=2000, 
+  control=list(max_treedepth = 15,adapt_delta = 0.99)
+)
+
+
+
+
+m_partpool_full <- map2stan(
+  alist(
+    resp ~ dnorm(mu, sigma),
+    mu <- a + a_species[species] + bf*force + bp*photo + bc*chill,
+    a_species[species] ~ dnorm(0, sigma_species),
+    a ~ dnorm(0, 50),
+    bf ~ dnorm(0,50),
+    bp ~ dnorm(0,50),
+    bc ~ dnorm(0,50),
+    sigma ~ dnorm(0,10),
+    sigma_species ~ dnorm(0,10)
+  ),
+  data=osp,
+  chains=4, iter=4000, warmup=2000, 
+  control=list(max_treedepth = 15,adapt_delta = 0.99)
+)
+
+m_partpool_full <- map2stan(
+  alist(
+    resp ~ dnorm(mu, sigma),
+    mu <- a + a_species[species] + a_study[study] + bf*force + bp*photo + bc*chill,
+    a_species[species] ~ dnorm(0, sigma_species),
+    a_study[study] ~ dnorm(0, sigma_study),
+    c(a, bf, bp, bc) ~ dnorm(0, 50),
+    c(sigma, sigma_species, sigma_study) ~ dnorm(0,10)
+  ),
+  data=osp,
+  chains=4, iter=4000, warmup=2000, 
+  control=list(max_treedepth = 15,adapt_delta = 0.99)
+)
+
 }
-
-
-
-if(FALSE){
-  m_partpool <- map2stan(
-    alist(
-      resp ~ dnorm(mu, sigma),
-      mu <- a[species] + bf*force + bp*photo + bc*chill,
-      a[species] ~ dnorm(a, sigma),
-      a ~ dnorm(0, 50),
-      bf ~ dnorm(0,50),
-      bp ~ dnorm(0,50),
-      bc ~ dnorm(0,50),
-      sigma ~ dnorm(0,10)
-    ),
-    data=osp,
-    chains=4, iter=2000, warmup=1500, 
-    control=list(max_treedepth = 15,adapt_delta = 0.99),
-  )
-  )
-}
-
